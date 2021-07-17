1bd26a95f092b5f90e8d1f9e31aab5de7621109a
sourav sinha <21f1001521@student.onlinedegree.iitm.ac.in>
2021-07-17 16:56:28 +0530

    problem2

--- problem2.py
+++ problem2.py
@@ -1,3 +1,26 @@
+'''
+Problem Statement -: In this even odd problem Given a range [low, high] (both inclusive), select K numbers from the range (a number can be chosen multiple times) such that sum of those K numbers is even.
+
+Calculate the number of all such permutations.
+
+As this number can be large, print it modulo (1e9 +7).
+
+Constraints
+
+0 <= low <= high <= 10^9
+K <= 10^6.
+Input
+
+First line contains two space separated integers denoting low and high respectively
+Second line contains a single integer K.
+Output
+
+Print a single integer denoting the number of all such permutations
+Time Limit
+
+1
+'''
+
 rangeOfvalues = input().split(" ")
 
 k = int(input())
